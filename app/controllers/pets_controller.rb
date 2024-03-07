2cfd4c7e528bc56bdb5e4d494f198fac457a0fb2
SakuraFuruya <cherryblossom.sakura.f@gmail.com>
2024-03-07 12:22:49 -0300

    modify new & crate for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,25 @@
 class PetsController < ApplicationController
-
   def new
-    @pets = Pet.find(params[:pet_id])
+    @tutor = Tutor.find(params[:tutor_id])
+    @pet = Pet.new
   end
 
   def create
+    @tutor = Tutor.find(params[:tutor_id])
+    @pet = Pet.new(pet_params)
+    @pet.tutor = @tutor
+
+    if @pet.save
+      redirect_to pets_path(@tutor)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :type, :breed, :birth)
   end
 
 end
