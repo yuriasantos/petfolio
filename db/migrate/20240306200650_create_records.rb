cb1ef9431c967485c600ffa71a50905caf69e62f
yuri santos <santosyurial@gmail.com>
2024-03-15 12:28:53 -0300

    Remove file and changed content type to text.

--- db/migrate/20240306200650_create_records.rb
+++ db/migrate/20240306200650_create_records.rb
@@ -1,8 +1,7 @@
 class CreateRecords < ActiveRecord::Migration[7.1]
   def change
     create_table :records do |t|
-      t.string :content
-      t.string :exams
+      t.text :content
       t.references :appointment, null: false, foreign_key: true
 
       t.timestamps
